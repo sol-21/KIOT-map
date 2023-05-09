2b0b3b06a8193bae979dff892a0a1eb68387f842
sol-21 <solomon17bdr@gmail.com>
2023-05-10 01:20:02 +0900

    adding live location and poly line

--- App.js
+++ App.js
@@ -11,253 +11,15 @@ import {
 	Text,
 	KeyboardAvoidingView
 } from 'react-native'
+import markers from './components/markers/markers'
 import SearchFilter from './components/SearchFilter'
-// import Icon from 'react-native-ionicons'
-import MapView, { Marker, MapTypes } from 'react-native-maps'
-const Images = [
-	{
-		uri: 'https://lh3.googleusercontent.com/p/AF1QipMfeojpM63DVnqA-5mUYCa3IVjvo4Wr0WyQqU8d=s1360-w1360-h1020'
-	},
-	{ uri: 'https://i.imgur.com/sNam9iJ.jpg' },
-	{ uri: 'https://i.imgur.com/sNam9iJ.jpg' },
-	{ uri: 'https://i.imgur.com/sNam9iJ.jpg' }
-]
+import * as Location from 'expo-location'
+import MapView, { Marker, MapTypes, Polyline } from 'react-native-maps'
+
 const { width, height } = Dimensions.get('window')
 const CARD_HEIGHT = 220
 const CARD_WIDTH = width * 0.8
 const SPACING_FOR_CARD_INSET = width * 0.1 - 10
-let markers = [
-	{
-		coordinate: {
-			latitude: 11.050598125259235,
-			longitude: 39.74936113917143
-		},
-		title: 'Dstv',
-		id: '0',
-		description: 'This is students Dstv',
-		image: Images[0]
-	},
-	{
-		coordinate: {
-			latitude: 11.051932335911083,
-			longitude: 39.74877221281011
-		},
-		title: 'Graduation Hall',
-		id: '1',
-		description: 'This is Graduation hall',
-		image: Images[0]
-	},
-	{
-		coordinate: {
-			latitude: 11.049191370049732,
-			longitude: 39.75009020332489
-		},
-		title: 'Gymnasium',
-		id: '2',
-		description: 'This is Gymnasium',
-		image: Images[0]
-	},
-	{
-		coordinate: {
-			latitude: 11.052409750780946,
-			longitude: 39.74680680588732
-		},
-		title: 'Administration',
-		id: '3',
-		description: 'This is Administration office',
-		image: Images[0]
-	},
-	{
-		coordinate: {
-			latitude: 11.049538214694493,
-			longitude: 39.74787795702361
-		},
-		title: 'Student Union',
-		id: '4',
-		description: 'This is Students Union ',
-		image: Images[0]
-	},
-	{
-		coordinate: {
-			latitude: 11.048806854002288,
-			longitude: 39.748136559691964
-		},
-		title: 'Shops & Photocopy',
-		id: '5',
-		description: 'This is Shops and Photocopy services',
-		image: Images[0]
-	},
-	{
-		coordinate: {
-			latitude: 11.051980952622452,
-			longitude: 39.74669892974005
-		},
-		title: 'Bank & ATM',
-		id: '6',
-		description: 'This is Administration office',
-		image: Images[0]
-	},
-	{
-		coordinate: {
-			latitude: 11.051917857755775,
-			longitude: 39.74954755161849
-		},
-		title: 'Cafeteria',
-		id: '7',
-		description: 'This is Administration office',
-		image: Images[0]
-	},
-	{
-		coordinate: {
-			latitude: 11.051719663305777,
-			longitude: 39.74774489462865
-		},
-		title: 'Informatics College',
-		id: '8',
-		description: 'This is Administration office',
-		image: Images[0]
-	},
-	{
-		coordinate: {
-			latitude: 11.049860472683585,
-			longitude: 39.750975168631776
-		},
-		title: 'White House',
-		id: '9',
-		description: 'This is Administration office',
-		image: Images[0]
-	},
-	{
-		coordinate: {
-			latitude: 11.054155277517383,
-			longitude: 39.74977981771949
-		},
-		title: 'Ancharo',
-		id: '10',
-		description: 'This is Administration office',
-		image: Images[0]
-	},
-	{
-		coordinate: {
-			latitude: 11.0484609310966,
-			longitude: 39.7501642678091
-		},
-		title: 'Informatics Laboratory',
-		id: '11',
-		description: 'This is Administration office',
-		image: Images[0]
-	},
-	{
-		coordinate: {
-			latitude: 11.0525979604587,
-			longitude: 39.747515374116546
-		},
-		title: 'Registrar',
-		id: '12',
-		description: 'This is Administration office',
-		image: Images[0]
-	},
-	{
-		coordinate: {
-			latitude: 11.048409865394955,
-			longitude: 39.74994340181729
-		},
-		title: 'Senior Males Dormitery',
-		id: '13',
-		description: 'This is Administration office',
-		image: Images[0]
-	},
-	{
-		coordinate: {
-			latitude: 11.048067640731928,
-			longitude: 39.74880614529703
-		},
-		title: 'Female Dormitery',
-		id: '14',
-		description: 'This is Administration office',
-		image: Images[0]
-	},
-	{
-		coordinate: {
-			latitude: 11.048667588186346,
-			longitude: 39.747775389216024
-		},
-		title: 'Sport Field',
-		id: '15',
-		description: 'This is Administration office',
-		image: Images[0]
-	},
-	{
-		coordinate: {
-			latitude: 11.050388206250837,
-			longitude: 39.74833564759589
-		},
-		title: 'Class Rooms',
-		id: '16',
-		description: 'This is Administration office',
-		image: Images[0]
-	},
-	{
-		coordinate: {
-			latitude: 11.051413659369077,
-			longitude: 39.74840629610825
-		},
-		title: 'Teachers Lounge',
-		id: '17',
-		description: 'This is Administration office',
-		image: Images[0]
-	},
-	{
-		coordinate: {
-			latitude: 11.049967737432604,
-			longitude: 39.7472694681964
-		},
-		title: 'Students Lounge',
-		id: '18',
-		description: 'This is Administration office',
-		image: Images[0]
-	},
-	{
-		coordinate: {
-			latitude: 11.050011876127659,
-			longitude: 39.747769422074825
-		},
-		title: 'Clinic',
-		id: '19',
-		description: 'This is Administration office',
-		image: Images[0]
-	},
-	{
-		coordinate: {
-			latitude: 11.048813881825973,
-			longitude: 39.748614049799855
-		},
-		title: 'Informatics Library',
-		id: '20',
-		description: 'This is Administration office',
-		image: Images[0]
-	},
-	{
-		coordinate: {
-			latitude: 11.049228373433188,
-			longitude: 39.748424284919764
-		},
-		title: 'Main Library',
-		id: '21',
-		description: 'This is Administration office',
-		image: Images[0]
-	},
-	{
-		coordinate: {
-			latitude: 11.05054792918316,
-			longitude: 39.747598179254
-		},
-		title: 'New Library',
-		id: '22',
-		description: 'This is Administration office',
-		image: Images[0]
-	}
-]
 
 const App = () => {
 	const initialMapState = {
@@ -274,6 +36,11 @@ const App = () => {
 	const [markerState, SetMarkerState] = useState(initialMapState)
 	const [input, setInput] = useState('')
 	const [selectedMarkerid, setSelectedMarkerid] = useState(null)
+
+	const [location, setLocation] = useState(initialMapState.region)
+	// const [region, setRegion] = useState(null)
+	const [polylineCoordinates, setPolylineCoordinates] = useState([])
+
 	let mapIndex = 0
 	let mapAnimation = new Animated.Value(0)
 
@@ -303,6 +70,37 @@ const App = () => {
 			}, 10)
 		})
 	})
+
+	useEffect(() => {
+		trackLocation()
+	}, [])
+
+	const trackLocation = async () => {
+		const { status } = await Location.requestForegroundPermissionsAsync()
+		if (status !== 'granted') {
+			console.log('Permission to access location was denied')
+			return
+		}
+
+		Location.watchPositionAsync(
+			{
+				accuracy: Location.Accuracy.High,
+				timeInterval: 1000,
+				distanceInterval: 4
+			},
+			(position) => {
+				// const { latitude, longitude } = position.coords
+				// const newLocation = { latitude, longitude }
+				// setLocation(newLocation)
+				// const newCoordinates = [...polylineCoordinates, { latitude, longitude }]
+				// setPolylineCoordinates(newCoordinates)
+				setLocation(position.coords)
+				setPolylineCoordinates((coords) => [...coords, position.coords])
+			}
+		)
+		// setLocation(location)
+	}
+
 	const interpolations = markerState.markers.map((marker, index) => {
 		const inputRange = [
 			(index - 1) * CARD_WIDTH,
@@ -320,6 +118,7 @@ const App = () => {
 	})
 	const onListPressed = () => {}
 	const onMarkerPress = (markerID) => {
+		console.log(markerID)
 		setSelectedMarkerid(Number(markerID))
 		let x = markerID * CARD_WIDTH + markerID * 20
 
@@ -363,16 +162,27 @@ const App = () => {
 
 	return (
 		<SafeAreaView style={{ flex: 1 }}>
-			<MapView
-				ref={_map}
-				style={{ alignSelf: 'stretch', height: '100%' }}
-				onRegionChange={onRegionChange}
-				region={markerState.region}
-				mapType={mapType}
-				onPress={toggleMapType}>
-				{showMarkers()}
-			</MapView>
-
+			{location && (
+				<MapView
+					ref={_map}
+					style={{ alignSelf: 'stretch', height: '100%' }}
+					region={markerState.region}
+					showsUserLocation={true}
+					followsUserLocation={true}
+					mapType={mapType}
+					onPress={toggleMapType}>
+					{polylineCoordinates.length > 0 && (
+						<Polyline
+							coordinates={polylineCoordinates}
+							strokeWidth={5}
+							strokeColor={'#f00'}
+							lineDashPattern={[1]}
+						/>
+					)}
+					{showMarkers()}
+					<Marker coordinate={location} />
+				</MapView>
+			)}
 			<View style={styles.searchBox}>
 				<TextInput
 					value={input}
@@ -400,19 +210,10 @@ const App = () => {
 				scrollEventThrottle={1}
 				showsHorizontalScrollIndicator={false}
 				style={styles.scrollView}
+				contentContainerStyle={styles.endPadding}
 				pagingEnabled
 				snapToInterval={CARD_WIDTH + 20}
 				snapToAlignment='center'
-				// contentInset={{
-				// 	top: 0,
-				// 	left: SPACING_FOR_CARD_INSET,
-				// 	bottom: 0,
-				// 	right: SPACING_FOR_CARD_INSET
-				// }}
-				// contentContainerStyle={{
-				// 	paddingHorizontal:
-				// 		Platform.OS === 'android' ? SPACING_FOR_CARD_INSET : 0
-				// }}
 				onScroll={Animated.event(
 					[
 						{
@@ -464,6 +265,9 @@ const styles = StyleSheet.create({
 		left: 0,
 		right: 0,
 		paddingVertical: 10
+	},
+	endPadding: {
+		paddingRight: width - CARD_WIDTH
 	},
 	card: {
 		elevation: 2,
---
+++ components/markers/markers.js
@@ -0,0 +1,243 @@
+const Images = [
+	{
+		uri: 'https://lh3.googleusercontent.com/p/AF1QipMfeojpM63DVnqA-5mUYCa3IVjvo4Wr0WyQqU8d=s1360-w1360-h1020'
+	},
+	{ uri: 'https://i.imgur.com/sNam9iJ.jpg' },
+	{ uri: 'https://i.imgur.com/sNam9iJ.jpg' },
+	{ uri: 'https://i.imgur.com/sNam9iJ.jpg' }
+]
+
+let markers = [
+	{
+		coordinate: {
+			latitude: 11.050598125259235,
+			longitude: 39.74936113917143
+		},
+		title: 'Dstv',
+		id: '0',
+		description: 'This is students Dstv',
+		image: Images[0]
+	},
+	{
+		coordinate: {
+			latitude: 11.051932335911083,
+			longitude: 39.74877221281011
+		},
+		title: 'Graduation Hall',
+		id: '1',
+		description: 'This is Graduation hall',
+		image: Images[0]
+	},
+	{
+		coordinate: {
+			latitude: 11.049191370049732,
+			longitude: 39.75009020332489
+		},
+		title: 'Gymnasium',
+		id: '2',
+		description: 'This is Gymnasium',
+		image: Images[0]
+	},
+	{
+		coordinate: {
+			latitude: 11.052409750780946,
+			longitude: 39.74680680588732
+		},
+		title: 'Administration',
+		id: '3',
+		description: 'This is Administration office',
+		image: Images[0]
+	},
+	{
+		coordinate: {
+			latitude: 11.049538214694493,
+			longitude: 39.74787795702361
+		},
+		title: 'Student Union',
+		id: '4',
+		description: 'This is Students Union ',
+		image: Images[0]
+	},
+	{
+		coordinate: {
+			latitude: 11.048806854002288,
+			longitude: 39.748136559691964
+		},
+		title: 'Shops & Photocopy',
+		id: '5',
+		description: 'This is Shops and Photocopy services',
+		image: Images[0]
+	},
+	{
+		coordinate: {
+			latitude: 11.051980952622452,
+			longitude: 39.74669892974005
+		},
+		title: 'Bank & ATM',
+		id: '6',
+		description: 'This is Administration office',
+		image: Images[0]
+	},
+	{
+		coordinate: {
+			latitude: 11.051917857755775,
+			longitude: 39.74954755161849
+		},
+		title: 'Cafeteria',
+		id: '7',
+		description: 'This is Administration office',
+		image: Images[0]
+	},
+	{
+		coordinate: {
+			latitude: 11.051719663305777,
+			longitude: 39.74774489462865
+		},
+		title: 'Informatics College',
+		id: '8',
+		description: 'This is Administration office',
+		image: Images[0]
+	},
+	{
+		coordinate: {
+			latitude: 11.049860472683585,
+			longitude: 39.750975168631776
+		},
+		title: 'White House',
+		id: '9',
+		description: 'This is Administration office',
+		image: Images[0]
+	},
+	{
+		coordinate: {
+			latitude: 11.054155277517383,
+			longitude: 39.74977981771949
+		},
+		title: 'Ancharo',
+		id: '10',
+		description: 'This is Administration office',
+		image: Images[0]
+	},
+	{
+		coordinate: {
+			latitude: 11.0484609310966,
+			longitude: 39.7501642678091
+		},
+		title: 'Informatics Laboratory',
+		id: '11',
+		description: 'This is Administration office',
+		image: Images[0]
+	},
+	{
+		coordinate: {
+			latitude: 11.0525979604587,
+			longitude: 39.747515374116546
+		},
+		title: 'Registrar',
+		id: '12',
+		description: 'This is Administration office',
+		image: Images[0]
+	},
+	{
+		coordinate: {
+			latitude: 11.048409865394955,
+			longitude: 39.74994340181729
+		},
+		title: 'Senior Males Dormitery',
+		id: '13',
+		description: 'This is Administration office',
+		image: Images[0]
+	},
+	{
+		coordinate: {
+			latitude: 11.048067640731928,
+			longitude: 39.74880614529703
+		},
+		title: 'Female Dormitery',
+		id: '14',
+		description: 'This is Administration office',
+		image: Images[0]
+	},
+	{
+		coordinate: {
+			latitude: 11.048667588186346,
+			longitude: 39.747775389216024
+		},
+		title: 'Sport Field',
+		id: '15',
+		description: 'This is Administration office',
+		image: Images[0]
+	},
+	{
+		coordinate: {
+			latitude: 11.050388206250837,
+			longitude: 39.74833564759589
+		},
+		title: 'Class Rooms',
+		id: '16',
+		description: 'This is Administration office',
+		image: Images[0]
+	},
+	{
+		coordinate: {
+			latitude: 11.051413659369077,
+			longitude: 39.74840629610825
+		},
+		title: 'Teachers Lounge',
+		id: '17',
+		description: 'This is Administration office',
+		image: Images[0]
+	},
+	{
+		coordinate: {
+			latitude: 11.049967737432604,
+			longitude: 39.7472694681964
+		},
+		title: 'Students Lounge',
+		id: '18',
+		description: 'This is Administration office',
+		image: Images[0]
+	},
+	{
+		coordinate: {
+			latitude: 11.050011876127659,
+			longitude: 39.747769422074825
+		},
+		title: 'Clinic',
+		id: '19',
+		description: 'This is Administration office',
+		image: Images[0]
+	},
+	{
+		coordinate: {
+			latitude: 11.048813881825973,
+			longitude: 39.748614049799855
+		},
+		title: 'Informatics Library',
+		id: '20',
+		description: 'This is Administration office',
+		image: Images[0]
+	},
+	{
+		coordinate: {
+			latitude: 11.049228373433188,
+			longitude: 39.748424284919764
+		},
+		title: 'Main Library',
+		id: '21',
+		description: 'This is Administration office',
+		image: Images[0]
+	},
+	{
+		coordinate: {
+			latitude: 11.05054792918316,
+			longitude: 39.747598179254
+		},
+		title: 'New Library',
+		id: '22',
+		description: 'This is Administration office',
+		image: Images[0]
+	}
+]
+
+export default markers
